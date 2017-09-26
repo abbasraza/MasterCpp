41d91356602f1bbfacebaae403646d88261da995
Abbas Raza <abbas.raza.1707@gmail.com>
2017-09-26 13:17:55 +1000

    class examples
    Signed-off-by: Abbas Raza <abbas.raza.1707@gmail.com>

---
+++ SimpleClass1/main.cpp
@@ -0,0 +1,49 @@
+//
+//  main.cpp
+//  SimpleClass1
+//
+//  Created by abbas raza on 26/09/2017.
+//  Copyright © 2017 Self. All rights reserved.
+//
+
+#include <iostream>
+
+class smallobj
+{
+private:
+	int data;
+public:
+	void set_data(int v) // will this be an inline function when object is instantiated??
+	{
+		data = v;
+	}
+	void show_data()
+	{
+		std::cout << "data = " << data << std::endl;
+	}
+	/*
+	void set_data(int v); // functions declared inside and defined outside
+	void show_data(); // functions declared inside and defined outside
+	 */
+};
+
+/*
+void smallobj::set_data(int v) // Will this be non inline function?
+{
+	data = v;
+}
+
+void smallobj::show_data()
+{
+	std::cout << "data = " << data << std::endl;
+}
+*/
+int main(int argc, const char * argv[])
+{
+	smallobj obj1;   // memory set aside for this object at this Object instantiation. functions memory??
+	// is obj1 a memory space or pointer to memory space???
+	obj1.show_data();
+	obj1.set_data(10);
+	obj1.show_data();
+    return 0;
+}
---
+++ circles/main.cpp
@@ -0,0 +1,58 @@
+//
+//  main.cpp
+//  circles
+//
+//  Created by abbas raza on 26/09/2017.
+//  Copyright © 2017 Self. All rights reserved.
+//
+
+#include <iostream>
+// circles.cpp
+// circles as graphics objects
+//#include "msoftcon.h"         // for graphics functions
+////////////////////////////////////////////////////////////////
+
+class circle
+{
+private:
+	int xCo, yCo;
+	int radius;
+	//color fillcolor;
+	//fstyle fillstyle;
+public:
+	void set(int x, int y, int r/*, color fc, fstyle fs*/)
+	{
+		xCo = x;
+		yCo = y;
+		radius = r;
+		//fillcolor = fc;
+		//fillstyle = fs;
+	}
+	void draw() {
+		//draws the circle
+		//set_color(fillcolor);
+		//set_fill_style(fillstyle);
+		//draw_circle(xCo, yCo, radius);
+		std::cout << "sorry no graphics support yet" << std::endl;
+	}
+	//set color
+	//set fill
+	//draw solid circle
+};
+
+int main()
+{
+	circle c1;
+	circle c2;
+	circle c3;
+	//initialize graphics system
+	//create circles
+	//set circle attributes
+	c1.set(15, 7, 5 /*,cBLUE, X_FILL*/);
+	c2.set(41, 12, 7 /*, cRED, O_FILL*/);
+	c3.set(65, 18, 4 /*, cGREEN, MEDIUM_FILL*/);
+	
+	c1.draw();
+	
+	return 0;
+}

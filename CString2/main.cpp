95adccad21d3cd313d09698177108b236b1841d5
Abbas Raza <abbas.raza.1707@gmail.com>
2017-10-07 15:59:30 +1100

    Signed-off-by: Abbas Raza <abbas.raza.1707@gmail.com>

--- CString2/main.cpp
+++ CString2/main.cpp
@@ -6,10 +6,24 @@
 //  Copyright © 2017 Self. All rights reserved.
 //
 
+/* Purpose of this program:
+ - Usage of std::cin.get function.
+ - How to get line with spaces.
+ - How to get paragraph.
+*/
+
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-	// insert code here...
-	std::cout << "Hello, World!\n";
-    return 0;
+int main(int argc, const char * argv[])
+{
+	char str[100];
+	std::cout << "Enter a line: ";
+	std::cin.get(str, 100);
+	std::cout << "You entered: " << str << std::endl;
+
+	std::cout << "Enter a Para: ";
+	std::cin.get(str, 100, '$');
+	std::cout << "You entered: " << str << std::endl;
+
+	return 0;
 }
---
+++ CStringCopy/main.cpp
@@ -0,0 +1,34 @@
+//
+//  main.cpp
+//  CStringCopy
+//
+//  Created by abbas raza on 7/10/2017.
+//  Copyright © 2017 Self. All rights reserved.
+//
+
+/* Purpose of this program:
+ - Usage of strcpy. strcpy appends null character to string at end. Normal copying doesn't.
+*/
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+	char str1[100];
+	char str2[100] = {[0 ... 99] = '?'};
+	char str3[] = "ABCD EFG HIJK LMN OP";
+
+	strcpy(str1, str3);
+
+	std::cout << "str1 = " << str1 << std::endl;
+
+	for (int i = 0; i < strlen(str3); ++i)
+	{
+		str2[i] = str3[i];
+	}
+
+	//	str2[strlen(str3)] = '\0'; //If not placed, whole string will be printed.
+	std::cout << "str2 = " << str2 << std::endl;
+
+	return 0;
+}

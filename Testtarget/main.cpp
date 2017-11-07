cdff090d618a2b01bbf3f3f06641af0a8d621341
Abbas Raza <abbas.raza.1707@gmail.com>
2017-11-07 13:45:12 +0500

    Signed-off-by: Abbas Raza <abbas.raza.1707@gmail.com>

---
+++ Testtarget/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Testtarget
+//
+//  Created by abbas raza on 7/11/17.
+//  Copyright © 2017 Self. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+    return 0;
+}
